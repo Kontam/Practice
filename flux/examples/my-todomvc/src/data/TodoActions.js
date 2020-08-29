30d10aa2c29b2812ed5c0a15705a4aa5ee587f82
Kanta <tehayan0111@gmail.com>
2020-08-29 11:27:29 +0900

    flux first commit

---
+++ flux/examples/my-todomvc/src/data/TodoActions.js
@@ -0,0 +1,11 @@
+import TodoActionTypes from './TodoActionTypes';
+import TodoDispatcher from './TodoDispatcher';
+
+const Actions = {
+  addTodo(text) {
+    TodoDispatcher.dispatch({
+      type: TodoActionType.ADD_TODO,
+      text,
+    });
+  },
+};
