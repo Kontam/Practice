e1590e0fcad2804f65593806a8659d68d48fb252
Kanta <tehayan0111@gmail.com>
2020-11-22 17:04:33 +0900

    fastify static対応

--- Redux/Vanila/src/server/server.js
+++ Redux/Vanila/src/server/server.js
@@ -1,4 +1,11 @@
 module.exports = async function (fastify, opts) {
+  const path = require('path');
+  console.log(process.cwd());
+
+  fastify.register(require('fastify-static'), {
+    root: path.join(process.cwd(), 'dist'),
+  })
+
   fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
   })
