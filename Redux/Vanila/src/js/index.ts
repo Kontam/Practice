df46cdccde4c3786641c059eb5a4398ebc840342
Kanta <tehayan0111@gmail.com>
2020-11-22 16:00:53 +0900

    parcelの導入

--- Redux/Vanila/src/js/index.ts
+++ Redux/Vanila/src/js/index.ts
@@ -1,9 +1,8 @@
 import { createStore } from 'redux';
 import { testFunc } from './util';
 
-
 (function main(){
+  const store = createStore(() => {});
+  console.log(store);
   testFunc();
-  const a = createStore((s, _) => s);
-  console.log(a);
 })();
