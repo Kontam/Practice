80239e39fb76848c5e4ebbcacc631f07915a8ad0
Kanta <tehayan0111@gmail.com>
2020-11-25 01:38:52 +0900

    バンドラーなしでts動かしつつreduxを使う方法を模索

---
+++ Redux/NoBundler/src/index.ts
@@ -0,0 +1,8 @@
+//import { testFunc } from './util';
+
+(function main(){
+  const body = document.querySelector('body');
+  const newOne = document.createElement('p');
+  newOne.innerText = 'wanko';
+  body.appendChild(newOne);
+})();
