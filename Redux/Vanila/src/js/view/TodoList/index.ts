e15d94356fd3483539e6f9d04803f2a603c54127
Kanta <tehayan0111@gmail.com>
2020-11-26 01:01:42 +0900

    connectedCallbackでならattributeが動作することを確認

--- Redux/Vanila/src/js/view/TodoList/index.ts
+++ Redux/Vanila/src/js/view/TodoList/index.ts
@@ -16,24 +16,28 @@ export const renderTodoList = (todos: Todos) => {
 export const createTodoListContainer = (todos: Todos) => {
   const containerElement = document.createElement("todo-list");
   containerElement.setAttribute('todos', JSON.stringify(todos));
-  console.log('todos', JSON.stringify(todos));
   return containerElement;
 }
 
 export class TodoListContainer extends HTMLElement {
+  shadow: ShadowRoot; 
   constructor() {
     super();
+    this.shadow = this.attachShadow({mode: 'open'});
+  }
+  
+  connectedCallback() {
     const todosString = this.getAttribute('todos');
     const todos: Todos = todosString ? JSON.parse(todosString) : [];
-//    console.log('parsed', todos, this, todosString);
-    console.log('attr', this.getAttributeNames());
-    
-    const shadow = this.attachShadow({mode: 'open'});
+    this.render(todos);
+  }
+
+  render(todos: Todos) {
     const containerElement = document.createElement("div");
     containerElement.setAttribute('data-js', 'List');
     const listElement = document.createElement("ul");
     
-    const todoItemElements = todos.map((todo) => {
+    const todoItemElements: HTMLElement[] = todos.map((todo) => {
       const item = document.createElement("li");
       const label = document.createElement("p");
       label.textContent = todo.name;
@@ -44,6 +48,6 @@ export class TodoListContainer extends HTMLElement {
     todoItemElements.forEach((element) => listElement.appendChild(element));
     containerElement.appendChild(listElement);
 
-    shadow.appendChild(containerElement);
+    this.shadow.appendChild(containerElement);
   }
 }
