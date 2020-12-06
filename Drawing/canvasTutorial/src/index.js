75ba35112f98062224dfb68f44267c0d5a6b011c
Kanta <tehayan0111@gmail.com>
2020-12-06 11:09:34 +0900

    tutorial drawText

--- Drawing/canvasTutorial/src/index.js
+++ Drawing/canvasTutorial/src/index.js
@@ -10,6 +10,37 @@ function drawRects(ctx) {
   ctx.clearRect(60,30,30,50);
 }
 
+function drawingTriangles(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.lineTo(75, 50);
+  ctx.stroke();
+}
+
+function drawFace(ctx) {
+  ctx.beginPath();
+  ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+  ctx.moveTo(110, 75);
+  ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+  ctx.moveTo(65, 65);
+  ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+  ctx.moveTo(95, 65);
+  ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+  ctx.stroke();
+}
+
+function drawText(ctx) {
+  ctx.font = '48px sans serif';
+  ctx.strokeText('Hello', 10, 50);
+}
+
+function measure(ctx) {
+  var text = ctx.measureText('nikonikonikoniko');
+  console.log(text);
+}
+
 
 function onLoad() {
   const CANVAS_ID = 'mycanvas';
@@ -20,6 +51,9 @@ function onLoad() {
     return;
   }
   const ctx = canvas.getContext('2d');
+  drawFace(ctx);
+  drawText(ctx);
+  measure(ctx);
 
 }
 
