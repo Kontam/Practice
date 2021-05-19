6a92d73754df338ddb26f413adb970f17a0f20e0
Kontam <tehayan0111@gmail.com>
2021-05-20 01:39:43 +0900

    add perf example

---
+++ Node/Performance/src/spawnPerf.ts
@@ -0,0 +1,23 @@
+import * as child_process from 'child_process';
+import { computeMedianRun } from 'lighthouse/lighthouse-core/lib/median-run.js';
+const lighthouseCli = require.resolve('lighthouse/lighthouse-cli');
+
+const results = [];
+for (let i = 0; i < 5; i++) {
+  console.log(`Running Lighthouse attempt #${i + 1}...`);
+    const {status = -1, stdout} = child_process.spawnSync('node', [
+          lighthouseCli,
+          'https://konkonta.com',
+          '--output=json'
+        ]);
+
+  if (status != 0) {
+    console.log('Lighthouse faild, skipping run...');
+    continue;
+  }
+  results.push(JSON.parse(stdout));
+}
+
+const median = computeMedianRun(results);
+console.log('Median performance score was', median.categories.performance.score * 100);
+
