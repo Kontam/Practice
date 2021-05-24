d1fc3ccc60d872d509dad9ff726733f276116583
Kontam <tehayan0111@gmail.com>
2021-05-24 23:12:33 +0900

    separate css

--- React/testing-lib/src/pages/divForm/index.tsx
+++ React/testing-lib/src/pages/divForm/index.tsx
@@ -1,60 +1,63 @@
-import React, {CSSProperties, useState} from 'react';
-
-const buttonStyle: CSSProperties = {
-  height: 30,
-  width: 60,
-  textAlign: 'center',
-  backgroundColor: 'gray',
-  color: 'white',
-};
+import React, { useState } from "react";
+import styles from "./divForm.module.css";
 
 const DivForm: React.FC = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [animals, setAnimals] = useState<string[]>([]);
-  const handleCheck: React.ChangeEventHandler<HTMLInputElement> = e => {
+  const [isDone, setIsDone] = useState(false);
+  const handleCheck: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     if (e.target.checked) {
       setAnimals([...animals, e.target.value]);
       return;
     }
-    setAnimals(animals.filter(animal => animal !== e.target.value));
+    setAnimals(animals.filter((animal) => animal !== e.target.value));
   };
-  const isDisabled = !name || setAnimals.length === 0;
+  const isDisabled = !name || animals.length === 0;
   // 入力されている名前とチェックされている動物をコンソールに出す
   const handleSubmit = () => {
-    if(isDisabled) return;
-    console.log('Submit', `name:${name}`, `animals:${animals.join(',')}`);
+    if (isDisabled) return;
+    console.log("Submit", `name:${name}`, `animals:${animals.join(",")}`);
+    setIsDone(true);
   };
 
   const checkList = [
-    {name: 'cat', label: 'Cat'},
-    {name: 'dog', label: 'Dog'},
-    {name: 'tiger', label: 'Tiger'},
+    { name: "cat", label: "Cat" },
+    { name: "dog", label: "Dog" },
+    { name: "tiger", label: "Tiger" },
   ];
   return (
     <div>
-      <div><strong>Test Form</strong></div>
-      <div>
-        <strong>name</strong>
-      </div>
-      <input type="text" name="text" onChange={e => setName(e.target.value)} />
+      <div className={styles.mainHeading}>Test Form</div>
+      <div className={styles.subHeading}>name</div>
+      <input
+        type="text"
+        name="text"
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <div style={{marginTop: 10}}>
+      <div className={styles.subHeading}>
         <strong>choose animals</strong>
       </div>
 
       {/* 動物のチェックボックスリスト */}
-      <div style={{display: 'flex'}}>
-        {checkList.map(item => (
-          <div key={item.name} style={{marginRight: 10}}>
+      <div className={styles.form}>
+        {checkList.map((item) => (
+          <div key={item.name} className={styles.item}>
             <div>{item.label}</div>
             <input type="checkbox" name={item.name} onChange={handleCheck} />
           </div>
         ))}
       </div>
 
-      <div style={buttonStyle} onClick={handleSubmit} data-disabled={isDisabled}>
+      <div
+        className={`${styles.button} ${
+          isDisabled ? styles.disabled : undefined
+        }`}
+        onClick={handleSubmit}
+      >
         submit
       </div>
+      {isDone && <div className={styles.success}>SUCCESS!!</div>}
     </div>
   );
 };
