d2623179523e1bb78f7a4a74241663c765700c48
Kontam <tehayan0111@gmail.com>
2021-05-31 01:29:31 +0900

    see git

--- Node/Performance/src/withGit.ts
+++ Node/Performance/src/withGit.ts
@@ -1,5 +1,7 @@
 import {readFileSync} from 'fs';
+import {execSync} from 'child_process';
 import {runLightHouse} from './perf';
+import {PerfConfig} from './types';
 
 (async () => {
   const targetStr = readFileSync('target.json', 'utf8');
@@ -8,6 +10,10 @@ import {runLightHouse} from './perf';
 
   const configStr = readFileSync('config.json', 'utf8');
   if (typeof configStr !== "string") return console.error("error", configStr);
-  const config = JSON.parse(configStr);
-  await runLightHouse(target[0], 'withGit.json');
+  const config = JSON.parse(configStr) as PerfConfig;
+  execSync(`cd ${config.gitRoot}`);
+  console.log(execSync('git status').toString());
+
+
+  // await runLightHouse(target[0], 'withGit.json');
 })();
