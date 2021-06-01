e9bbca8242643902b512f0b60907bc7ab220c6b2
Kontam <tehayan0111@gmail.com>
2021-06-02 00:45:03 +0900

    add chdir

--- Node/Performance/src/withGit.ts
+++ Node/Performance/src/withGit.ts
@@ -5,6 +5,7 @@ import {PerfConfig} from './types';
 import { format } from 'date-fns';
 
 (async () => {
+  const CWD = process.cwd();
   const DATE_FORMAT = 'yyyyMMdd-hhmmss';
   const targetStr = readFileSync('target.json', 'utf8');
   if (typeof targetStr !== "string") return console.error("error", targetStr);
@@ -14,11 +15,12 @@ import { format } from 'date-fns';
   if (typeof configStr !== "string") return console.error("error", configStr);
   const config = JSON.parse(configStr) as PerfConfig;
   // Todo: ディレクトリ移動
-  const gitLogResult = execSync(`cd ${config.gitRoot} & git log | grep commit -m1`).toString();
-  console.log(gitLogResult);
+  process.chdir(config.gitRoot);
+  const gitLogResult = execSync(`git log | grep commit -m1`).toString();
   const lastCommitHash = gitLogResult.split(' ')[1];
 
   const timeStamp = format(new Date(), DATE_FORMAT);
 
-  //await runLightHouse(target[0], `${lastCommitHash}-${timeStamp}.json`);
+  process.chdir(CWD);
+  await runLightHouse(target[0], `${lastCommitHash}_${timeStamp}.json`);
 })();
