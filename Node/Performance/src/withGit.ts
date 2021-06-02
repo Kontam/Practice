c3fb4807103cf4a86596d43ef2a8403ec3d4382e
Kontam <tehayan0111@gmail.com>
2021-06-03 01:35:02 +0900

    wip withgit

--- Node/Performance/src/withGit.ts
+++ Node/Performance/src/withGit.ts
@@ -4,17 +4,22 @@ import {runLightHouse} from './perf';
 import {PerfConfig} from './types';
 import { format } from 'date-fns';
 
+type Target = {
+  name: string,
+  path: string,
+}
+
 (async () => {
   const CWD = process.cwd();
   const DATE_FORMAT = 'yyyyMMdd-hhmmss';
   const targetStr = readFileSync('target.json', 'utf8');
   if (typeof targetStr !== "string") return console.error("error", targetStr);
-  const target = JSON.parse(targetStr);
+  const target:Target[] = JSON.parse(targetStr);
 
   const configStr = readFileSync('config.json', 'utf8');
   if (typeof configStr !== "string") return console.error("error", configStr);
   const config = JSON.parse(configStr) as PerfConfig;
-  // Todo: ディレクトリ移動
+
   process.chdir(config.gitRoot);
   const gitLogResult = execSync(`git log | grep commit -m1`).toString();
   const lastCommitHash = gitLogResult.split(' ')[1];
@@ -22,5 +27,8 @@ import { format } from 'date-fns';
   const timeStamp = format(new Date(), DATE_FORMAT);
 
   process.chdir(CWD);
-  await runLightHouse(target[0], `${lastCommitHash}_${timeStamp}.json`, 'dist');
+  // TODO: promiseを順番に実行する　並列でエラーが出ているらしい
+  const promises = target.map((t) => {
+    return runLightHouse(t.path, `${lastCommitHash}_${timeStamp}.json`, `dist/${t.name}`);
+  });
 })();
