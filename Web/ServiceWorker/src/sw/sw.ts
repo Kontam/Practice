6728ec31e0cb7486b3d43351a4cf87dc40540093
Kontam <tehayan0111@gmail.com>
2021-07-17 22:37:39 +0900

    see fetch event

--- Web/ServiceWorker/src/sw/sw.ts
+++ Web/ServiceWorker/src/sw/sw.ts
@@ -1,8 +1,17 @@
+declare const self: ServiceWorker;
+
 self.addEventListener('install', function (event) {
   // Perform install steps
   console.log('sw.js');
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event: any) {
   console.log(event);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      console.log(response);
+    })
+  );
 });
+
+export {};
