8214f0c3f4fce8926b5fa3d424307d82b62696ba
Kontam <tehayan0111@gmail.com>
2021-07-18 01:00:17 +0900

    add image to Index

--- Web/ServiceWorker/src/pages/index.tsx
+++ Web/ServiceWorker/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
+import Image from 'next/image';
 import Header from '../components/organisms/Header';
+import momo from '../../public/images/momo.png';
+import junco from '../../public/images/junco.png';
 
 const Index: NextPage = () => {
   return (
@@ -9,6 +12,14 @@ const Index: NextPage = () => {
       <div>
         <h1>home</h1>
         <p>This is index.tsx</p>
+        <div style={{ display: 'flex' }}>
+          <div style={{ position: 'relative', height: 500, width: 500 }}>
+            <Image src={momo} alt="momo" objectFit="contain" layout="fill" />
+          </div>
+          <div style={{ position: 'relative', height: 500, width: 500 }}>
+            <Image src={junco} alt="junco" objectFit="contain" layout="fill" />
+          </div>
+        </div>
       </div>
     </div>
   );
