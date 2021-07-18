c3982c54a2091ab8053c770f65ac4a4e3fdf630f
Kontam <tehayan0111@gmail.com>
2021-07-18 22:56:46 +0900

    add image cache

--- Web/ServiceWorker/src/sw/sw.ts
+++ Web/ServiceWorker/src/sw/sw.ts
@@ -1,17 +1,27 @@
-declare const self: ServiceWorker;
+// declare const self: ServiceWorker;
 
-self.addEventListener('install', function (event) {
+const CHACHE_NAME = 'my-site-cachev1';
+const urlsToCache = ['/images/junco.png'];
+
+self.addEventListener('install', function (event: any) {
   // Perform install steps
   console.log('sw.js');
+  event.waitUntil(
+    caches.open(CHACHE_NAME).then((cache) => {
+      console.log('opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', function (event: any) {
   console.log(event);
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      console.log(response);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
-
-export {};
