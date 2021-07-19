ee2d342c4aeb91ca9b565f301c86ad99d0ec4b8d
Kontam <tehayan0111@gmail.com>
2021-07-20 01:26:02 +0900

    add pwa

---
+++ Web/ServiceWorker/src/components/organisms/Head/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Head: React.FC = () => {
+  return (
+    <head>
+      <link rel="manifest" href="manifest.json" />
+    </head>
+  );
+};
+
+export default Head;
