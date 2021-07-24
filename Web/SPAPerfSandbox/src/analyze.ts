7d40897468296b312dfb09ae39cba90b3abdd273
Kontam <tehayan0111@gmail.com>
2021-07-24 20:33:44 +0900

    add image diff check

--- Web/SPAPerfSandbox/src/analyze.ts
+++ Web/SPAPerfSandbox/src/analyze.ts
@@ -3,35 +3,46 @@ import colors from 'colors';
 import perfJson from '../test.json';
 import * as fs from 'fs-extra';
 import {SnapShot} from '../types';
+import {imgDiff} from './jpgImgDiff';
 
 const OUT_DIR = './dist';
 
-let snapshots: SnapShot[] = perfJson.traceEvents.filter((data, index) => {
-  if ('snapshot' in data.args) {
-    return data;
-  }
-});
+(async () => {
+  let snapshots: SnapShot[] = perfJson.traceEvents.filter((data, index) => {
+    if ('snapshot' in data.args) {
+      return data;
+    }
+  });
 
-console.log('length', snapshots.length);
+  console.log('length', snapshots.length);
 
-snapshots.forEach((data, index) => {
-  fs.outputFile(
-    `${OUT_DIR}/${index}.jpeg`,
-    data.args.snapshot,
-    {encoding: 'base64'},
-    err => {
-      console.error('outputFile', err);
-    },
-  );
-  console.log(index, data.ts);
-});
-/*
-const diff = Diff.diffChars(first, second);
-console.log(diff);
+  // 画像ファイル書き出し
+  snapshots.forEach((data, index) => {
+    fs.outputFile(
+      `${OUT_DIR}/${index}.jpeg`,
+      data.args.snapshot,
+      {encoding: 'base64'},
+      err => {
+        console.error('outputFile', err);
+      },
+    );
+    console.log(index, data.ts);
+  });
 
-diff.forEach((part: any) => {
-  const c = part.added ? 'green':
-   part.removed ? 'red' : 'grey'; 
-  process.stderr.write(part.value[c]);
-});
-*/
+  const final = `${OUT_DIR}/${snapshots.length - 1}.jpeg`;
+
+  let firstMismatchedIndex = -1;
+  // 最後の１枚と画像ファイルを後ろから順番に比較
+  const p = snapshots.map(async (data, index) => {
+    const currentIndex = snapshots.length - 1 - index;
+    const result = await imgDiff(final, `${OUT_DIR}/${currentIndex}.jpeg`);
+    if (result !== 0 && firstMismatchedIndex === -1) {
+      firstMismatchedIndex = currentIndex; // 最初に差分が出た画像のインデックス
+    }
+    return;
+  });
+
+  await Promise.all(p);
+  console.log('index:', firstMismatchedIndex);
+  console.log('diff!!', final,  `${OUT_DIR}/${firstMismatchedIndex}.jpeg`);
+})();
--- Web/SPAPerfSandbox/src/jpgImgDiff.ts
+++ Web/SPAPerfSandbox/src/jpgImgDiff.ts
@@ -2,7 +2,7 @@ import pixelmatch from 'pixelmatch';
 import fs from 'fs';
 import JPEG from 'jpeg-js';
 
-export async function imgDiff(imgPath1: string, imgPath2: string) {
+export async function imgDiff(imgPath1: string, imgPath2: string): Promise<number> {
   const img1JpegData = fs.readFileSync(imgPath1);
   const img1 = JPEG.decode(img1JpegData);
   const img2JpegData = fs.readFileSync(imgPath2);
@@ -10,15 +10,18 @@ export async function imgDiff(imgPath1: string, imgPath2: string) {
   const diff: any = {data: new Buffer(img1.width * img1.height * 4), width: img1.width, height: img1.height};
 
   const result = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {
-    threshold: 5,
+    threshold: 0.3,
   });
 
   console.log('result', result);
-  console.log('diff', diff);
   const jpegImgData = JPEG.encode(diff, 50);
   fs.writeFileSync('image.jpg', jpegImgData.data);
+  return result;
 }
 
+// test
+/*
 (async () => {
-  await imgDiff('./dist/1.jpeg', './dist/2.jpeg');
+  imgDiff('./dist/1.jpeg', './dist/2.jpeg');
 })();
+*/
