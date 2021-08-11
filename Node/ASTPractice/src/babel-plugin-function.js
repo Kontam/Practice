a68f214e62afeef87a3ed2d41d90d5ff81fe6cc6
Kontam <tehayan0111@gmail.com>
2021-08-11 19:55:19 +0900

    using state

--- Node/ASTPractice/src/babel-plugin-function.js
+++ Node/ASTPractice/src/babel-plugin-function.js
@@ -1,4 +1,5 @@
 const {transform} = require('@babel/core');
+const syntaxTypeScript = require('@babel/plugin-syntax-typescript').default;
 
 const plugin = babel => {
   const {traverse, types: t, template, version} = babel;
@@ -7,15 +8,25 @@ const plugin = babel => {
 
   return {
     name: 'my-plugin-name',
-    pre: () => { console.log('pre, before traverse')},
-    post: () => { console.log('post, after traverse')},
+    inherits: syntaxTypeScript,
+    pre() {
+      // thisはvisitor関数の第二引数になる thisを使うのでアロー関数ではだめ
+      this.hoge = 'hoge';
+      console.log('pre, before traverse');
+    },
+    post() {
+      console.log('post, after traverse', this.hoge);
+    },
     visitor: {
-      BinaryExpression: nodePath => {
+      Program: (nodePath, state) => {
+        state.hoge = 'ほげ';
+      },
+      BinaryExpression: (nodePath, state) => {
         console.log(t.isExpression(nodePath));
         console.log(nodePath.node.operator);
-      }
-    }
-  }
+      },
+    },
+  };
 };
 
-transform('1+2', {plugins: [plugin]});
+transform('let a: number = 1+2', {plugins: [plugin]});
