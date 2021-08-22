d479eef73a7b1d47306547e1a12a35d75719a8e8
Kontam <tehayan0111@gmail.com>
2021-08-23 01:23:42 +0900

    add ssr,static

--- React/next-class/my-app/pages/about.tsx
+++ React/next-class/my-app/pages/about.tsx
@@ -1,12 +1,22 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const About: NextPage = () => {
+  const [messages, setMessage] = useState<any[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/messages")
+      .then((result) => {
+        return result.json();
+      })
+      .then((json) => setMessage(json));
+  }, []);
+
   return (
     <div>
       <h1>About us</h1>
-      <p>Hello World</p>
+      <p>{messages.map(m => m.body)}</p>
     </div>
   );
-}
+};
 
 export default About;
