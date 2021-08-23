dc305ffbd461689d515b70e86f6d5a0dcf6228f8
Kontam <tehayan0111@gmail.com>
2021-08-24 00:21:26 +0900

    feat: add menu

---
+++ React/next-class/my-app/pages/menu.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import Link from "next/link";
+
+const Menu: NextPage = () => {
+  return (
+    <div>
+	    <ul>
+        <li>
+          <Link href="/about" passHref><a>About</a></Link>
+        </li>
+        <li>
+          <Link href="/about_ssr" passHref><a>About_SSR</a></Link>
+        </li>
+        <li>
+          <Link href="/about_static" passHref><a>About_Static</a></Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
