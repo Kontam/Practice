9e6db08051c34d317c5ad2d02370262246155ceb
Kontam <tehayan0111@gmail.com>
2021-08-30 01:29:04 +0900

    doing handson

---
+++ React/next-class/test-app-name/pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default About;
