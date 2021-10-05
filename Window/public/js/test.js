3dc508882f034e6f600afd12ea6978d9a0e76ce8
Kontam <tehayan0111@gmail.com>
2021-10-05 21:34:59 +0900

    append

--- Window/public/js/test.js
+++ Window/public/js/test.js
@@ -3,8 +3,17 @@
   const button = document.getElementById("test");
   if (!button) return console.error("no button");
 
+  const appedButton = document.getElementById("append");
+  if (!appedButton) return console.error("no appedButton");
+
+  let child;
   button.addEventListener("click", () => {
-    window.open("../html/second.html", "testName", "width=400, height=400");
+    child = window.open("../html/second.html", "testName", "width=400, height=400");
   })
+  
+  appedButton.addEventListener("click", () => {
+    child.document.getElementById("head").innerHTML = "from parent";
+  })
+
 
 })()
