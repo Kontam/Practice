7147ce5efa5c155822be1df08bb7118a2c614eab
Kontam <tehayan0111@gmail.com>
2021-11-11 00:57:44 +0900

    using command

--- Node/CommanderTest/src/index.ts
+++ Node/CommanderTest/src/index.ts
@@ -2,12 +2,24 @@ import {Command} from 'commander';
 
 function main() {
   const program = new Command();
-  program.option('-d, --debug', 'output extra debugging');
-  program.option('-t, --test', 'testing default value', 'default');
+  //program.option('-d, --dublle', 'output extra');
+  //program.option('-t, --test', 'testing default value', 'default');
+
+  program.command('echo <st>')
+  .option('-d, --dublle', 'output extra')
+  .description('say something')
+  .action((st:string, options: any) => {
+    console.log('ehco action', st, options);
+    if (options.dublle) {
+      console.log("echo dubble", options);
+    }
+  });
 
   program.parse(process.argv);
   const options = program.opts();
   console.log(options);
+
+
 }
 
 main();
