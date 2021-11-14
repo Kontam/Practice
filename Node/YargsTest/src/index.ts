2ba869c719d789a6e971c4d49dd25a33e7e57afa
Kontam <tehayan0111@gmail.com>
2021-11-14 21:35:28 +0900

    using command

--- Node/YargsTest/src/index.ts
+++ Node/YargsTest/src/index.ts
@@ -1,7 +1,12 @@
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
-const argv = yargs(hideBin(process.argv)).argv;
+const yrgs = yargs(hideBin(process.argv));
+yrgs.command('serve [port]', 'start the server', (yargs) => {
+  return yargs.positional('port', {describe: 'port to bind on', default: 5000,});
+});
+
+const argv = yrgs.argv;
 
 console.log(argv);
 
