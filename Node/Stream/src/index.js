cb04d94cc389f5894af9e349737ff8579d87d462
Kontam <tehayan0111@gmail.com>
2021-11-20 20:38:54 +0900

    trying large one

--- Node/Stream/src/index.js
+++ Node/Stream/src/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
-const src = fs.createReadStream('src.txt', 'utf8');
+const src = fs.createReadStream('/Users/kanta/Downloads/RJ298251.zip', 'utf8');
 const dest = fs.createWriteStream('dest.text', 'utf8');
 
 src.on('data', chunk => {
   console.log('chunk', chunk);
-  dest.write(chunk)
+  //dest.write(chunk)
 });
 src.on('end', () => dest.end());
+
