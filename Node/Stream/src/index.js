c427a1dea21c3f71a2b6ef7ab00ae3159823481b
Kontam <tehayan0111@gmail.com>
2021-11-21 20:02:34 +0900

    console every baffer

--- Node/Stream/src/index.js
+++ Node/Stream/src/index.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const src = fs.createReadStream('/Users/kanta/Downloads/RJ298251.zip', 'utf8');
 const dest = fs.createWriteStream('dest.text', 'utf8');
 
+let count = 0;
 src.on('data', chunk => {
-  console.log('chunk', chunk);
+  count++;
+  console.log('count:', count);
   //dest.write(chunk)
 });
 src.on('end', () => dest.end());
