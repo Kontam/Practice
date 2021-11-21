acc36fa9fb0adf9df30c88da54e0115e04ae319f
Kontam <tehayan0111@gmail.com>
2021-11-22 00:32:52 +0900

    testing high water mark

--- Node/Stream/src/index.js
+++ Node/Stream/src/index.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 
-const src = fs.createReadStream('/Users/kanta/Downloads/RJ298251.zip', 'utf8');
-const dest = fs.createWriteStream('dest.text', 'utf8');
+const src = fs.createReadStream('src.txt', {
+  encoding: 'utf8',
+  highWaterMark: 1,
+});
+const dest = fs.createWriteStream('dest.txt', 'utf8');
 
 let count = 0;
 src.on('data', chunk => {
   count++;
   console.log('count:', count);
+  console.log('chunk:', chunk);
   //dest.write(chunk)
 });
 src.on('end', () => dest.end());
-
