d5e3aa89dd6ce70773c4c66b48f692b912086d78
Kontam <tehayan0111@gmail.com>
2021-11-23 20:08:21 +0900

    using config

---
+++ Web/ChromeExtension/src/config.ts
@@ -0,0 +1,27 @@
+declare const chrome:any;
+
+function saveConfig() {
+  const apiKeyField = document.getElementById("APIKey");
+  if (!apiKeyField) return console.error("form not found");
+  chrome.storage.local.set({
+    apiKey: (apiKeyField as HTMLInputElement).value,
+  });
+  window.alert("Saved!!");
+}
+
+function restoreConfig() {
+  const apiKey = document.getElementById("APIKey");
+  chrome.storage.local.get({
+    apiKey
+  }, (values: any) => {
+    if(!apiKey) return console.error("no form");
+    (apiKey as HTMLInputElement).value = values.apiKey;
+  });
+  window.alert("Saved!!");
+
+}
+
+(() => {
+  document.getElementById("save")?.addEventListener("click", saveConfig);
+  document.addEventListener("DOMContentLoaded", restoreConfig);
+})();
