abd6415a5c9f5f2e902e48c2a86a1db1ef2a381a
Kontam <tehayan0111@gmail.com>
2021-12-08 01:39:43 +0900

    add kyoto

--- timeline/CSRTimeline/scenario/kyoto.js
+++ timeline/CSRTimeline/scenario/kyoto.js
@@ -1,11 +1,11 @@
 const { Scenario } = require('rendering-timer');
 
 module.exports = new Scenario({
-  name: 'tokyo/index_to_kurashi',
-  outDir: 'tokyo/index_to_kurashi',
-  startUrl: 'https://www.metro.tokyo.lg.jp/index.html',
+  name: 'kyoto/index_to_news',
+  outDir: 'kyoto/index_to_news',
+  startUrl: 'https://www.pref.kyoto.jp/index.html',
   triger: async (page) => {
-    const link = await page.$('#tmp_glist2');
+    const link = await page.$('[href="/news/index.html"]');
     if(!link) return console.error('link was not found')
     await link.click();
   } 
